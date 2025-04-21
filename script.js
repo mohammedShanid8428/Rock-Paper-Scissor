a372081a6ae7d3412809fea89a190f2d3a319ad8
mohammedshanid <123456789+odin@users.noreply.github.com>
2025-04-21 18:36:04 +0000

    script.js all function are added to index.html

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getComputerChoice() {
   else if (choice == 2) {
     return "papper"
   }
-  else (choice == 3){
+  if (choice == 3) {
     return "scissor"
 
   }
@@ -16,13 +16,13 @@ function getComputerChoice() {
 const rockBtn = document.getElementById("rock")
 const papperBtn = document.getElementById("papper")
 const scissorBtn = document.getElementById("scissor")
-const playerScore = document.getElementById("player-score")
-const computerScore = document.getElementById("computer-score")
+const playerScoreUpdate = document.getElementById("player-score")
+const computerScoreUpdate = document.getElementById("computer-score")
 const resultTxt = document.getElementById("result")
 
 rockBtn.addEventListener("click", () => getHumanChoice("rock"))
 papperBtn.addEventListener("click", () => getHumanChoice("papper"))
-papperBtn.addEventListener("click", () => getHumanChoice("scissor"))
+scissorBtn.addEventListener("click", () => getHumanChoice("scissor"))
 
 function getHumanChoice(humanSelection) {
   const computerSelection = getComputerChoice();
@@ -44,15 +44,37 @@ function playRound(humanChoice, computerChoice) {
     resultTxt.textContent = "You Lose this Round";
     computerScore++;
     playerScoreUpdate.textContent = humanScore;
-    playerScoreUpdate.textContent = computerScore;
+    computerScoreUpdate.textContent = computerScore;
 
   }
   else if (
     (humanChoice == "rock" && computerChoice == "scissor") ||
     (humanChoice == "papper" && computerChoice == "rock") ||
-    (humanChoice == "scissor" && computerChoice == "papper") 
-    
-  )
+    (humanChoice == "scissor" && computerChoice == "papper")){
+      resultTxt.textContent = "You Won this Round";
+      humanScore++;
+      playerScoreUpdate.textContent = humanScore;
+      computerScoreUpdate.textContent = computerScore;
+  
+    }
    
   
+}
+
+//to check if player has won this game//
+
+if (humanScore == 5) {
+  resultTxt.textContent = "You Have Won The Game. Your Total Score is" + humanScore + "to" + computerScore + ",congragulations.";
+  resetGame();
+}
+else if (computerScore == 5) {
+  resultTxt.textContent = "You Have LOst The Game. Your Total Score is" + humanScore + "to" + computerScore + ",Better Luck Next TIme.";
+  resetGame()
+}
+
+function resetGame() {
+  humanScore = 0;
+  computerScore = 0;
+  playerScoreUpdate.textContent = humanScore;
+  computerScoreUpdate.textContent = computerScore;
 }
